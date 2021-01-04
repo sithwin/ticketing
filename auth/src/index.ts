651b2b44a95ba00ed5438f3d838d4383f3462bde
Si Thu Win <sthuwin@gmail.com>
2021-01-04 21:18:20 +0800

    added skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentUser', (request, response) => {
+  response.status(200).send("Hey and Hay THAR KHIT!");
+});
+
 app.listen(3000, () => {
-  console.log('Auth Service Listening on port 3000!')
-})
+  console.log('(Auth Service) Listening on port 3000!')
+});
