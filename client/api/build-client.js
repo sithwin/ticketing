2ffa018eb27d1caa6e8c0ce4fff6c381f6609570
Si Thu Win <sthuwin@gmail.com>
2021-01-30 13:40:14 +0800

    add nav bar

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-export default (req) => {
+export default ({ req }) => {
   if (typeof window === "undefined") {
-    console.log("We are on the server");
+    // We are on the server
+
     return axios.create({
       baseURL: "http://ingress-nginx.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
-    console.log("We are on the browser");
+    // We must be on the browser
     return axios.create({
-      baseURL: "/", // optional
+      baseUrl: "/",
     });
   }
 };
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,15 +2,17 @@ import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   return currentUser ? (
-    <h1>Your are signed in</h1>
+    <h1>You are signed in</h1>
   ) : (
-    <h1>You are not signed in</h1>
+    <h1>You are NOT signed in</h1>
   );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  const client = buildClient(req);
-  const { data } = await client.get("api/users/currentUser");
+LandingPage.getInitialProps = async (context) => {
+  console.log("LANDING PAGE!");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
   return data;
 };
 
