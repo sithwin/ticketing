7931b6270b3553178717bdab83989212fbc22f92
Si Thu Win <sthuwin@gmail.com>
2021-02-03 22:27:40 +0800

    created initial project

---
+++ tickets/src/index.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined!");
+  }
+
+  try {
+    //await mongoose.connect("mongodb://localhost:27017/auth", {
+    await mongoose.connect("mongodb://auth-mongo-service:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log("Connected to mongoDB!!");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("(Auth Service) Listening on port 3000!");
+  });
+};
+
+start();
