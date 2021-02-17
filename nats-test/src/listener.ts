0bf9cc53467791c346c3fd74a0b3a36d0420201c
Si Thu Win <sthuwin@gmail.com>
2021-02-17 23:07:55 +0800

    abstract listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
 console.clear();
@@ -19,11 +19,11 @@ stan.on("connect", () => {
     .subscriptionOptions()
     .setManualAckMode(true)
     .setDeliverAllAvailable()
-    .setDurableName("order-service");
+    .setDurableName("order-service-channel");
 
   const subscription = stan.subscribe(
     "ticket:created",
-    "order-service-queue-group",
+    "order-service-channel",
     options
   );
 
@@ -40,3 +40,47 @@ stan.on("connect", () => {
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, message: Message): void;
+
+  private client: Stan;
+  protected ackWait = 5 * 1000; // 5 second
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (message: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(message);
+      this.onMessage(parsedData, message);
+    });
+  }
+
+  parseMessage(message: Message) {
+    const data = message.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+}
