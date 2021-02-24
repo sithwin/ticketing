09d3ff5faea3c0e2965ee1ac139b1c8e231b8a61
Si Thu Win <sthuwin@gmail.com>
2021-02-24 21:59:06 +0800

    import routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFoundError } from "@vmticketing/common";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,6 +18,10 @@ app.use(
   })
 );
 app.use(currentUser);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
