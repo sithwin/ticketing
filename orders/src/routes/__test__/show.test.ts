4cbc63b20f449343c87f7d594515f2597daf8a4a
Si Thu Win <sthuwin@gmail.com>
2021-03-01 22:11:03 +0800

    get order by id

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,50 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetch the order", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.order.id);
+});
+
+it("return an error if one user tries to fetch another users order", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
