9d344faa02ef477bec52d44077ffed9052a1c09c
Si Thu Win <sthuwin@gmail.com>
2021-03-04 22:01:40 +0800

    publish order created event

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("return an error if the ticket does not exist", async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -49,4 +50,20 @@ it("reserves a ticket", async () => {
     .set("Cookie", global.signin())
     .send({ ticketId: ticket.id })
     .expect(201);
+});
+
+it("emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
